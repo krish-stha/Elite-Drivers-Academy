c126b467b277d20cfc50273954c012b23a4d655e
krish shrestha <143826247+krish-stha@users.noreply.github.com>
2024-06-07 13:07:40 +0545

    Add files via upload

---
+++ UtsavFirst.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package utsavfirst;
+
+import utsavfirst.View.StartScreen;
+
+/**
+ *
+ * @author utsav
+ */
+public class UtsavFirst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StartScreen ss = new StartScreen();
+        ss.setVisible(true);
+        
+    }
+    
+}
