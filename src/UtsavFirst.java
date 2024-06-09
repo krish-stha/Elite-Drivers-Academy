0e1fe9823f5e3829d1645a403da600f316edee7c
krish <krishstha202@gmail.com>
2024-06-10 00:52:18 +0900

    few changes

---
+++ src/UtsavFirst.java
@@ -0,0 +1,24 @@
+
+import utsavfirst.View.log;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 97798
+ */
+public class UtsavFirst {
+
+    public static void main(String[] args) {
+//         Create instances of Login and signup classes
+        log login = new log();
+        login.setVisible(true);
+//
+     
+    }
+}
+
